ccf7a2cfcaf5c87ba0fa1693f2ac8ec0b2530c30
repnop <repnop@repnop.dev>
2020-11-16 21:56:56 -0500

    build(build.rs): Remove virt feature stuff from buildscript

--- src/vanadinite/build.rs
+++ src/vanadinite/build.rs
@@ -4,8 +4,6 @@
 
 use std::fs::read_dir;
 
-const VIRT: &str = "CARGO_FEATURE_VIRT";
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     for file in read_dir("lds")? {
         let file = file?;
@@ -15,11 +13,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         println!("cargo:rerun-if-changed={}", file.file_name().into_string().unwrap());
-    }
-
-    match std::env::var(VIRT) {
-        Ok(_) => println!(r#"cargo:rustc-cfg=feature="virt""#),
-        Err(_) => {}
     }
 
     Ok(())
