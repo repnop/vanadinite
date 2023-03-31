f31649fe7c3da50378687feb7c4cfa9cf6199aa4
repnop <repnop@repnop.dev>
2023-03-30 20:41:41 -0400

    fix(vanadinite): Fix deadlock bug

--- src/kernel/vanadinite/src/syscall/io.rs
+++ src/kernel/vanadinite/src/syscall/io.rs
@@ -110,9 +110,10 @@ pub fn claim_device(task: &Task, regs: &mut GeneralRegisters) -> Result<(), Sysc
 
                             task_state.claimed_interrupts.insert(id, HART_ID.get());
                             // FIXME: not sure if this is entirely correct..
-                            task_state
-                                .kernel_channel
-                                .sender
+                            let sender = task_state.kernel_channel.sender.clone();
+                            drop(task_state);
+
+                            sender
                                 .send(ChannelMessage {
                                     data: Into::into(KernelMessage::InterruptOccurred(id)),
                                     caps: Vec::new(),
